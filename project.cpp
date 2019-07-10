23ac850d8dbb506492c7c144ec1b5b533e442f9f
fra-nguyen-thanh-cong <nguyen.thanh.cong@framgia.com>
2019-07-10 19:19:13 +0700

    10-07-2019

--- project.cpp
+++ project.cpp
@@ -1,10 +1,20 @@
-#include<iostream>
-#include<sqlite3.h>
+#include <iostream>
+#include <sqlite3.h>
 
-#include"sqlite.h"
+#include "sqlite.h"
 
 using namespace std;
 
-int main(){
+int main()
+{
+    sqlite3_stmt *stmt;
+    sqlite3_prepare(db, pSQL, -1, &stmt, NULL);
+
+    while (sqlite3_step(stmt) == SQLITE_ROW)
+    {
+        cout << sqlite3_column_text(stmt, 0) << endl;
+        cout << sqlite3_column_text(stmt, 1) << endl;
+        cout << sqlite3_column_text(stmt, 2) << endl;
+    }
     return 0;
 }
